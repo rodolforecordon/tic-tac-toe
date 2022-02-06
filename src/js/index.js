3a9a16f93af7026fd8c1d72ce07f7181c3cbee42
rodolforecordon <rodolforecordon@gmail.com>
2022-02-05 23:56:33 -0300

    changed css units

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,27 @@ let currentTurn = '';
 
 const mainContainer = document.querySelector('#main-container');
 
-mainContainer.addEventListener('click', (e) => {
+mainContainer.addEventListener('click', e => {
   const elId = e.target.id;
   document.getElementById(elId).innerText = 'X';
-})
+});
 
-function Player(name, mark) {
-  const getName = () => name;
-  const getMark = () => mark;  
+const mainDialog = document.querySelector('#main-dialog');
+const startButton = document.querySelector('#confirm-btn');
+
+startButton.addEventListener('click', e => {
+  mainDialog.close();
+});
+
+// function Game
+function Game() {
+  
 }
 
-//function Player
+// function Player
+function Player(name, color, marker) {
+  const getName = () => name;
+  const getColor = () => color;
+  const getMark = () => marker;
+}
+
